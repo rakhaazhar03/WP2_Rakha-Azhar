b8585035124861f6ab6118e260dfcbca7af92c80
rakhaazhar03 <167833464+rakhaazhar03@users.noreply.github.com>
2024-05-07 08:57:11 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama Saya Rakha Azhar Qawsaini
+    Saya tinggal di Pamulang
+    Hobby saya adalah main";
+    }
+}
